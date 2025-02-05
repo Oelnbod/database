de2367027bbff12a463cafe606822431e176003b
joel body <joel.body@proton.me>
2025-02-05 16:24:09 +0000

    added comments

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,27 @@ pub mod schema;
 use self::models::*;
 
 fn main() {
-    use schema::passwords::dsl::{website, passwords};
-    
+    use schema::passwords::dsl::{passwords, website};
+
     let connection = &mut connect_to_db();
 
+    // the order of what follows is just for testing and can be changed
+
+    //displaying the results
     let results = display_database(connection);
     println!("{:?}", results);
-    
-    let _password_out = create_password(connection, "example.com", "example", "hi");
 
+    //writing the password, it returns the row added (I think)
+    let password_out = create_password(connection, "example.com", "example", "hi");
+
+    //displaying the results
     let result = display_database(connection);
-    println!("after \n");
-    println!("{:?}", result);
+    println!("\n {:?}", result);
 
+    //deleting the database
     delete_from_database("example.com", connection);
-    
+
+    //displaying the database again.
     println!("\n \n {:?}", display_database(connection));
 }
 
@@ -29,22 +35,21 @@ fn connect_to_db() -> SqliteConnection {
     SqliteConnection::establish("main.db").unwrap_or_else(|_| panic!("Error connecting to main.db"))
 }
 
+//reading the database
 fn display_database(connection: &mut SqliteConnection) -> Vec<Password> {
     use schema::passwords::dsl::passwords;
     let result: Vec<Password> = passwords
         .load(connection)
-        .expect("error connecting to database"); // reading the db
+        .expect("error connecting to database");
     result
-
 }
-
+//creating a new entry
 fn create_password(
     conn: &mut SqliteConnection,
     website: &str,
     username: &str,
     password_value: &str,
 ) -> Password {
-    
     use crate::schema::passwords;
     let new_password_data = NewPassword {
         website,
@@ -59,6 +64,7 @@ fn create_password(
         .expect("Error saving password data")
 }
 
+//deleting the database
 fn delete_from_database(target: &str, connection: SqliteConnection) {
     let num_deleted = diesel::delete(passwords.filter(website.like(pattern)))
         .execute(connection)
