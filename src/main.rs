af4dc7446b75be3d17ac2170abd2ac48e00fb6d8
joel body <joel.body@proton.me>
2025-02-08 15:19:40 +0000

    implemented basic http web server with threadpool that only responds with a message.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,36 @@ pub mod database;
 
 //using serde for parsing json
 use serde_json;
+use std::net::{TcpListener, TcpStream};
+use std::io::*;
+use std::thread;
+use threadpool::ThreadPool;
 
 fn main() {
-    //connecting to the database
-    let connection = &mut database::connect();
+    let thread_pool = ThreadPool::new(10);
+
+    thread::spawn(move || {
+    let http_listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    for stream in http_listener.incoming() {
+        match stream {
+            Ok(stream) => {
+		thread_pool.execute(move || {
+		    handle_connection(stream)
+		});
+	    },		    
+            Err(_) => {
+		println!("Failed to connect to request.");
+	    }
+	} 
+    }
+    });
+
+    //prevents the main thread from ending (for multithreading)
+    loop {
+	thread::park();
+    }
+    /* database stuff (commented out whilst api/web server stuff implemented)
+    let connection = &mut database::connect(); //connecting to the database
 
     // --- the order of what follows is just for testing and can be changed --
 
@@ -29,4 +55,41 @@ fn main() {
 
     //displaying the database again.
     println!("\n \n {:?}", database::display(connection));
+     */
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    //reading the request
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    // this next bit returns the bit after the /  in the http request
+
+  //  let file = file_target(&request_line); //this is the file but after the slash that was entered
+
+    //let target = return_file(file.to_string());
+
+    //let mut full_file: String = "./www/".to_string();
+    //full_file.push_str(&target); //full_file is the file including the ./www/ bit
+    //println!("{full_file}");
+
+    if request_line == "GET / HTTP/1.1" {
+        //this is different so that the index.html is the landing page
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = "hi";
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = "hi";
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+
+        //note: there is no 404.html exception as this is handled by return_file()
+    }
 }
